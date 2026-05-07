0074b141c380ad60848e50afeb00109a3c738fc6
SharifMuhammadSifat <166907907+SharifMuhammadSifat@users.noreply.github.com>
2026-05-07 16:05:21 +0600

    Chnaging page while keeping navbar as it is

--- keen-keeper/src/app/page.js
+++ keen-keeper/src/app/page.js
@@ -4,7 +4,7 @@ import Navbar from "../Components/Navbar/Navbar";
 export default function Home() {
   return (
     <section>
-      <Navbar></Navbar>
+      
       <h1>Welcome to the Home Page</h1>
     </section>
   );
