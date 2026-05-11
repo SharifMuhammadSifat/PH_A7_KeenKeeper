fb4266c308586bb0e8af17ab78ee9ec415e67647
SharifMuhammadSifat <166907907+SharifMuhammadSifat@users.noreply.github.com>
2026-05-11 16:31:37 +0600

    friends card

--- keen-keeper/src/Components/friendCard/Friendcard.jsx
+++ keen-keeper/src/Components/friendCard/Friendcard.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Friendcard = ({ friend }) => {
     return (
         <div className='p-6 flex flex-col items-center gap-4 bg-white shadow-md rounded-sm'>
-            <Image className='w-20 h-20 rounded-full' src={friend.picture} alt={friend.name} width={800} height={800}/>
+            <Image className='w-20 h-20 rounded-full' src={friend.picture} alt={friend.name} width={800} height={800} />
             <h2>{friend.name}</h2>
             <p className='text-[#64748B] text-[12px]'>{friend.days_since_contact}d ago</p>
             <div className='flex flex-wrap gap-2'>
@@ -14,9 +14,11 @@ const Friendcard = ({ friend }) => {
                     ))
                 }
             </div>
-            <div className={`${friend.status === 'on-track' ? 'bg-green-500' : friend.status === 'overdue' ? 'bg-red-500' : 'bg-yellow-500' } text-[12px] font-regular text-white px-1 py-1 rounded-full`}>
-                {friend.status}
-            </div>
+            <button>
+                <div className={`${friend.status === 'on-track' ? 'bg-green-500' : friend.status === 'overdue' ? 'bg-red-500' : 'bg-yellow-500'} text-[12px] font-regular text-white px-1 py-1 rounded-full`}>
+                    {friend.status}
+                </div>
+            </button>
         </div>
     );
 };
